04d399f27bd54d2e44351398703bad7f536dca1e
Dmitry <mioakiyama245@gmail.com>
2021-01-06 21:32:13 +1000

    Тестирую пост процессинг

---
+++ Assets/Scripts/aa_CharacterController.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aa_CharacterController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Animator animator;
+    public VariableJoystick joystick;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+    public float turnSmoothTime = 0.1f;
+    public float pushPower = 2.0F;
+    float turnSmoothVelocity;
+
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //animator.SetFloat("HorizontalVelosity", Mathf.Abs(Input.GetAxis("Horizontal")));
+        animator.SetFloat("HorizontalVelosity", Mathf.Abs(joystick.Horizontal));
+
+        MovementLogic();
+
+        JumpLogic();
+
+    }
+
+    private void MovementLogic()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        //Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 direction = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
+        controller.Move(direction * Time.deltaTime * playerSpeed);
+
+        //if (direction != Vector3.zero)
+        //{
+        //    gameObject.transform.forward = direction;
+        //}
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            controller.Move(direction * playerSpeed * Time.deltaTime);
+
+        }
+    }
+
+    public void JumpButtonLogic()
+    {
+        if (groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            controller.Move(playerVelocity * Time.deltaTime);
+        }
+    }
+
+
+    public void JumpLogic()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (groundedPlayer)
+            {
+                playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+                controller.Move(playerVelocity * Time.deltaTime);
+            }
+        }
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+
+
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+
+        // no rigidbody
+        if (body == null || body.isKinematic)
+            return;
+
+        // We dont want to push objects below us
+        if (hit.moveDirection.y < -0.3f)
+            return;
+
+        // Calculate push direction from move direction,
+        // we only push objects to the sides never up and down
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+        // If you know how fast your character is trying to move,
+        // then you can also multiply the push velocity by that.
+
+        // Apply the push
+        body.AddForce(pushDir * pushPower);
+    }
+
+}
